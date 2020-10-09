c6d905fd6f147f4cefc186c37d71cb0505026ac0
Emeka-Chukwu <emekapascal11@gmail.com>
2020-10-09 13:04:23 +0100

    made changes to the main and pubspec lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:fiverly_flutter/features/carts_order/screen/carts.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'features/shipping_details/bloc/my_order_counter_bloc.dart';
@@ -14,17 +13,11 @@ void main() async {
     ],
     child: MyApp(),
   ));
-=======
-import 'package:fixing/features/authentication/sign_up.dart';
-import 'features/authentication/forgot_password.dart';
-import 'features/authentication/login.dart';
-import 'package:fixing/features/successpages/success1.dart';
-
-import 'features/successpages/success2.dart';
-void main() {
-  runApp(MyApp());
->>>>>>> 49e20ff6b8e300caa74c143caa68ad1de2c07835
 }
+// void main() {
+//   runApp(MyApp());
+// >>>>>>> 49e20ff6b8e300caa74c143caa68ad1de2c07835
+// }
 
 class MyApp extends StatelessWidget {
   @override
@@ -35,9 +28,9 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(  
+      home: Scaffold(
         backgroundColor: Color(0xffe5e5e5),
-        body:SuccessPage2(),
+        body: Carts(),
       ),
     );
   }
